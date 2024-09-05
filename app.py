b44bd878e35209963eeee9ec3fd99676149f93fd
Francisco Flores <is728238@iteso.mx>
2024-09-05 15:01:45 -0600

    Functional API

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 from flask import Flask, render_template, request
 import requests
+import os
+from dotenv import load_dotenv
+
+# Cargar las variables del archivo .env
+load_dotenv()
 
 app = Flask(__name__)
 
-# Reemplaza con tu API Key de TMDb
-API_KEY = 'TU_API_KEY'
+# Obtener la API key desde el archivo .env
+API_KEY = os.getenv('API_KEY')
 
-# Ruta para la página principal
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# Ruta para manejar la búsqueda de películas
 @app.route('/search', methods=['POST'])
 def search():
     movie_name = request.form.get('movie_name')
     url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={movie_name}"
     response = requests.get(url).json()
 
-    # Extraemos resultados de la búsqueda
     movies = response.get('results', [])
     return render_template('home.html', movies=movies, movie_name=movie_name)
 
